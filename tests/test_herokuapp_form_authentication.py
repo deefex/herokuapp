302b615bfb8ebff2a53fb4057bdbad2735b8690e
Del Dewar <del.dewar@skyscanner.net>
2016-11-18 08:47:35 +0000

    modified failure scenarios

--- tests/test_herokuapp_form_authentication.py
+++ tests/test_herokuapp_form_authentication.py
@@ -21,12 +21,14 @@ class LoginTest(unittest.TestCase):
         assert secure_area_page.is_loaded(secure_area_page.SECURE_AREA_TITLE), "Login secure area failed to load"
 
     def test_form_authentication_invalid_username(self):
-        login_page = LoginPage(self.driver).attempt_login("bobsmith", "SuperSecretPassword!")
+        login_page = LoginPage(self.driver)
+        login_page.attempt_login("bobsmith", "SuperSecretPassword!")
         assert login_page.login_error_displayed()
         assert "Your username is invalid!" in self.driver.page_source
 
     def test_form_authentication_invalid_password(self):
-        login_page = LoginPage(self.driver).attempt_login("tomsmith", "NotMyPassword!")
+        login_page = LoginPage(self.driver)
+        login_page.attempt_login("tomsmith", "NotMyPassword!")
         assert login_page.login_error_displayed()
         assert "Your password is invalid!" in self.driver.page_source
 
