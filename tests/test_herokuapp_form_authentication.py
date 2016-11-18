c713df026a0fc43fee0e62687a12e64a15ae2ecd
Del Dewar <del.dewar@skyscanner.net>
2016-11-18 08:34:37 +0000

    Condensed tests even further

--- tests/test_herokuapp_form_authentication.py
+++ tests/test_herokuapp_form_authentication.py
@@ -16,20 +16,17 @@ class LoginTest(unittest.TestCase):
         self.driver.quit()
 
     def test_form_authentication_successful_login(self):
-        login_page = LoginPage(self.driver)
-        login_page.attempt_login("tomsmith", "SuperSecretPassword!")
+        LoginPage(self.driver).attempt_login("tomsmith", "SuperSecretPassword!")
         secure_area_page = SecureAreaPage(self.driver)
         assert secure_area_page.is_loaded(secure_area_page.SECURE_AREA_TITLE), "Login secure area failed to load"
 
     def test_form_authentication_invalid_username(self):
-        login_page = LoginPage(self.driver)
-        login_page.attempt_login("bobsmith", "SuperSecretPassword!")
+        login_page = LoginPage(self.driver).attempt_login("bobsmith", "SuperSecretPassword!")
         assert login_page.login_error_displayed()
         assert "Your username is invalid!" in self.driver.page_source
 
     def test_form_authentication_invalid_password(self):
-        login_page = LoginPage(self.driver)
-        login_page.attempt_login("tomsmith", "NotMyPassword!")
+        login_page = LoginPage(self.driver).attempt_login("tomsmith", "NotMyPassword!")
         assert login_page.login_error_displayed()
         assert "Your password is invalid!" in self.driver.page_source
 
